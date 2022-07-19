cf5045d3eb24fc557fc04521dd500105cf852f20
lcy <cymarch98@gmail.com>
2022-07-19 19:30:45 +0900

    ✨ 유저 모델/서비스/라우터
    - 아이디로 유저 찾기
    - 유저 닉네임 수정
    - 유저 정보 음식데이터 배열에 음식아이디 추가
    - 유저탈퇴

--- src/db/models/user-model.js
+++ src/db/models/user-model.js
@@ -6,16 +6,43 @@ const User = model("user", UserSchema);
 
 export class UserModel{
     
-    async create(userInfo) {
-        const createdNewUser = await User.create(userInfo);
+    async create(newUserInfo) {
+        const createdNewUser = await User.create(newUserInfo);
         return createdNewUser;
     }
 
-    async findOne(email) {
-        const user = await User.findOne(email);
+    async findOne(userInfo) {
+        const user = await User.findOne(userInfo);
         return user;
     }
+
+    async findById(userId){
+        const idUser = await User.findById(userId);
+        return idUser;
+    }
+
+    async updateNick(userId, newNick) {
+        const updatedUser = await User.findOneAndUpdate(
+            {_id: userId},
+            {$set: {nickname: newNick}},
+            {returnOriginal: false}
+        );
+        return updatedUser;
+    }
     
+    async updateUserFood(userId, newFoodId) {
+        const updateUser = await User.findOneAndUpdate(
+            {_id: userId},
+            {$push: {foodData: {foodId: newFoodId}}}, // user스키마 foodData배열에 새로 추천받은 음식 아이디 추가
+            {returnOriginal: false}
+        );
+        return updateUser;
+    }
+
+    async deleteUser(userId){
+        const deleteUser = await User.deleteOne(userId);
+        return deleteUser;
+    }
 
 }
 
--- src/routers/user-router.js
+++ src/routers/user-router.js
@@ -10,4 +10,38 @@ userRouter.post("/", async(req, res) => {
     res.status(201).json(newUser);
 });
 
+userRouter.get('/', async(req, res, next) => {
+    const userId = req.currentUserId;
+    
+    try{
+        const user = await userService.getUser(userId);
+        res.status(200).json(user);
+    }catch(err){
+        next(err);
+    }
+});
+
+userRouter.patch('/nickname', async(req, res, next) => {
+    const {userId, newNickname} = req.body;
+
+    try{
+        const updateNickname = await userService.editUserNickname(userId, newNickname);
+        res.status(200).json(updateNickname);
+    }catch(err){
+        next(err);
+    }
+    
+});
+
+userRouter.patch('/addFood', loginRequired, async(req, res, next) => {
+    const {userId, addFoodId} = req.body;
+
+    try{
+        const updateUserFood = await userService.addUserFood(userId, addFoodId);
+        res.status(200).json(updateUserFood);
+    }catch(err){
+        next(err);
+    }
+});
+
 export { userRouter };
