790048662b0b5531438415c6194e42dd7ba8b99d
lcy <cymarch98@gmail.com>
2022-07-19 15:30:11 +0900

    🎨 유저 토큰 생성

--- src/routers/auth-router.js
+++ src/routers/auth-router.js
@@ -1,5 +1,6 @@
 import { Router } from "express";
 import passport from "passport";
+import { setUserToken } from '../utils/index.js';
 
 const authRouter = Router();
 
@@ -34,8 +35,12 @@ authRouter.get('/google', passport.authenticate('google', {scope: ['profile', 'e
 authRouter.get(
    '/google/callback', 
    passport.authenticate('google',{session:false}), //그리고 passport 로그인 전략에 의해 googleStrategy로 가서 구글계정 정보와 DB를 비교해서 회원가입시키거나 로그인 처리하게 한다
-   (req, res) => {
-      res.redirect('/');
+   async (req, res, next) => {
+      try{
+         setUserToken(req.user, res);
+      }catch(err){
+         next(err);
+      }
    }
 );
 
