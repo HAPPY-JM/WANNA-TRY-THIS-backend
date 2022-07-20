99c2bdd2522ed392c2099b7886afac5fef9f924e
lcy <cymarch98@gmail.com>
2022-07-21 01:11:50 +0900

    ✨ food라우터 - 대답을 body로 받아 음식 get하는 라우터 생성

--- src/routers/food-router.js
+++ src/routers/food-router.js
@@ -16,6 +16,22 @@ foodRouter.get("/", async (req, res) => {
     res.status(200).json(getFoods);
 });
 
+foodRouter.get("/result", async(req, res) => {
+    const {mood, age, money, ingredient} = req.body;
+    const answersToFilter = {
+        $or: [ // or? and?
+            { mood: { $in: mood } }, // in을 쓰는게 맞는지 시험해볼것!!
+            { age: { $in: age } },
+            { money: { $in: money } },
+            { ingredient: { $in: ingredient } }
+        ]
+    }
+
+    const filteredFoods = await foodService.foodFilter(answersToFilter);
+
+    res.status(200).json(filteredFoods);
+})
+
 
 
 export {foodRouter};
