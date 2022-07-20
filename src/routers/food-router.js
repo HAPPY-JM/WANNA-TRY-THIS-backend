7949ab4d19e19b35a6cde5f20913f77108f7440a
lcy <cymarch98@gmail.com>
2022-07-21 01:14:03 +0900

    💡 food라우터 주석 추가 - 각 라우터들이 어떤 라우터인지 간략하게

--- src/routers/food-router.js
+++ src/routers/food-router.js
@@ -3,6 +3,7 @@ import { foodService } from '../services/index.js';
 
 const foodRouter = Router();
 
+//음식추가
 foodRouter.post("/", async(req, res) => {
     const foodInfo = req.body;
     const addNewFood = await foodService.addFood(foodInfo);
@@ -10,12 +11,14 @@ foodRouter.post("/", async(req, res) => {
     res.status(201).json(addNewFood);
 });
 
+//모든음식get
 foodRouter.get("/", async (req, res) => {
     const getFoods = await foodService.findAll();
 
     res.status(200).json(getFoods);
 });
 
+//필터링음식get
 foodRouter.get("/result", async(req, res) => {
     const {mood, age, money, ingredient} = req.body;
     const answersToFilter = {
