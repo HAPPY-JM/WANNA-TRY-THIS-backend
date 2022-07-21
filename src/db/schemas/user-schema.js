ec8735702947dfe7e73dedec42f9b197bf213857
lcy <cymarch98@gmail.com>
2022-07-21 16:02:45 +0900

    🎨 유저 스키마/모델/라우터 수정
    - populate
    - 포스트맨 확인을 위한 loginRequired 주석처리
    - 유저아이디 선언 수정
    - 음식데이터 부분 수정

--- src/db/schemas/user-schema.js
+++ src/db/schemas/user-schema.js
@@ -14,16 +14,15 @@ const UserSchema = new Schema(
             // ref: (받아온 정보의 이메일)
         },
 
-        foodData: { //음식 확정할때마다 추가
-            type: [
-                {
-                    foodId: {
-                        type: Schema.Types.ObjectId,
-                        ref: "food",
-                    },
+        foodData: [ //음식 확정할때마다 추가
+            {
+                foodId: {
+                    type: Schema.Types.ObjectId,
+                    ref: "food",
                 },
-            ],
-        },
+            }
+        ],
+
         // { kakao, google, naver }
         provider: {
             type: String,
