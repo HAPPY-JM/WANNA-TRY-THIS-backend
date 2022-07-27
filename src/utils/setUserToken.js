ff36a333b5df3c51a7e336fa176dbabdaaf82bc3
lcy <cymarch98@gmail.com>
2022-07-27 21:41:55 +0900

    🚑 로그인 오류 수정중!

--- src/utils/setUserToken.js
+++ src/utils/setUserToken.js
@@ -3,5 +3,5 @@ import jwt from 'jsonwebtoken';
 export const setUserToken = (user, res) => {
 	const { nickname, email, provider } = user;
 	const token = jwt.sign({ nickname, email, provider }, process.env.JWT_SECRET);
-	res.status(200).json({ token });
+	res.redirect(`localhost:3000/Main`);
 };
