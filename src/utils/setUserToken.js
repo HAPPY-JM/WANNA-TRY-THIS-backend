2221c682023985cb7712c40d57c7bbdbc38ec247
lcy <cymarch98@gmail.com>
2022-07-29 00:41:20 +0900

    🎨 setUserToken: 리다이렉트 주소 변경
    - 'http://localhost:3000/' => 'http://localhost:3000/EmptyPage/'

--- src/utils/setUserToken.js
+++ src/utils/setUserToken.js
@@ -3,8 +3,6 @@ import jwt from 'jsonwebtoken';
 export const setUserToken = (user, res) => {
 	const { nickname, email, provider } = user;
 	const token = jwt.sign({ nickname, email, provider }, process.env.JWT_SECRET);
-	res.cookie('jwtToken', token, {
-		httpOnly: true,
-	});
-	res.redirect(`http://localhost:3000/`);
+	res.cookie('jwtToken', token, { httpOnly: true });
+	res.redirect(`http://localhost:3000/EmptyPage/`);
 };
