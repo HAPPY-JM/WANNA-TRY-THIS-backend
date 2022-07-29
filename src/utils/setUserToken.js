e832b34356badf432bf43865a6b2a11fbbe8ac11
jeongmin SEO <all.the.lights.on.me@gmail.com>
2022-07-29 20:53:03 +0900

    🔧💄🚧 긴급수정

--- src/utils/setUserToken.js
+++ src/utils/setUserToken.js
@@ -3,7 +3,5 @@ import jwt from 'jsonwebtoken';
 export const setUserToken = (user, res) => {
 	const { nickname, email, provider } = user;
 	const token = jwt.sign({ nickname, email, provider }, process.env.JWT_SECRET);
-	res.cookie('jwtToken', token, {
-		httpOnly: true,
-	});
+	res.cookie('jwtToken', token);
 };
