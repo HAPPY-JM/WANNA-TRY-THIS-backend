358d80b35c427c7726c615f87f2b150158816f58
lcy <cymarch98@gmail.com>
2022-07-29 23:32:23 +0900

    🎨 setUserToken 수정

--- src/utils/setUserToken.js
+++ src/utils/setUserToken.js
@@ -3,6 +3,5 @@ import jwt from 'jsonwebtoken';
 export const setUserToken = (user, res) => {
 	const { nickname, email, provider } = user;
 	const token = jwt.sign({ nickname, email, provider }, process.env.JWT_SECRET);
-	res.cookie('jwtToken', token, { httpOnly: true });
-	res.redirect(`http://localhost:3000/EmptyPage/`);
+	res.cookie('jwtToken', token);
 };
